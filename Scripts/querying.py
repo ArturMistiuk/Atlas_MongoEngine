a50565c4b5495a5e64fcadf39e55cd94655967f5
Artur Mistiuk <mistiuk.artur@gmail.com>
2023-05-22 15:22:04 +0200

    Update querying.py

--- Scripts/querying.py
+++ Scripts/querying.py
@@ -1,3 +1,5 @@
+import sys
+
 from connect import connection
 from models import Author, Quote
 
@@ -5,10 +7,25 @@ from models import Author, Quote
 def search_quote_from_name(user_args):
     name = user_args.split('name:')[1]
     # get obj Author from name to be sure everything is correct
-    author = Author.objects(fullname=name).first()
-    quotes = Quote.objects(author=author.id)
+    author = Author.objects.get(fullname=name)
+    quotes = Quote.objects(author=author)
     for quote in quotes:
-        print(quote)
+        print(quote.quote)
+
+
+def search_quote_from_tag(user_args):
+    tag_name = user_args.split('tag:')[1]
+    quotes = Quote.objects.filter(tags__contains=tag_name)
+    for quote in quotes:
+        print(quote.quote)
+
+
+def search_quote_from_tags(user_args):
+    tags_names = user_args.split('tags:')[1]
+    tags_list = tags_names.split(',')
+    quotes = Quote.objects.filter(tags__in=tags_list)
+    for quote in quotes:
+        print(quote.quote)
 
 
 def handle_argument(argument):
@@ -18,6 +35,8 @@ def handle_argument(argument):
         return search_quote_from_tag
     elif argument.startswith('tags:'):
         return search_quote_from_tags
+    elif argument == 'exit':
+        sys.exit()
     else:
         print('Unknown command!')
 
